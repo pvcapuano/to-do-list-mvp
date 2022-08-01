e909752b5a184e8f00454d135482ae04e8fd103e
pvcapuano <pvcapuano@gmail.com>
2022-08-01 20:19:39 -0300

    add localstorage

--- src/components/ListInput/index.jsx
+++ src/components/ListInput/index.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ListItems from "../ListItems";
 import { Container, Wrapper, Input, Button } from "./styles";
 
 const ListInput = () => {
+  const [posts, setPosts] = useState([
+    () => {
+      const savedPosts = localStorage.getItem("posts");
+
+      if (posts) {
+        return JSON.parse(savedPosts);
+      } else {
+        return [];
+      }
+    },
+  ]);
+
+  useEffect(() => {
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts]);
+
+  
   return (
     <Container>
       <h1>O que deseja adicionar na lista?</h1>
